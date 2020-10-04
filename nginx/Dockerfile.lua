44f027956f111099b376d526e3c823ac97af762c
aogg <aogg@users.noreply.github.com>
2020-10-04 23:36:54 +0800

    Update Dockerfile.lua

--- nginx/Dockerfile.lua
+++ nginx/Dockerfile.lua
@@ -1,5 +1,5 @@
 FROM nginx
 
 RUN apt-get update \
-      && apt-get install -y --no-install-recommends libnginx-mod-http-lua \
+      && apt-get install -y --no-install-recommends libnginx-mod-http-lua/bionic-updates \
       && rm -rf /var/lib/apt/lists/* 
