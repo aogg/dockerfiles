8fc49d3c06a74f11f6ca223fb67b9eef96d01a1f
aogg <aogg@users.noreply.github.com>
2020-11-07 22:36:26 +0800

    add -force

--- whistle/proxy/docker_cmd.sh
+++ whistle/proxy/docker_cmd.sh
@@ -8,6 +8,6 @@ w2 start $MORE_OPTIONS -p $PROXY_PORT -P $UI_PORT \
     -n $ADMIN_USERNAME -w $ADMIN_PASSWORD \
     -N $GUEST_USERNAME -W $GUEST_PASSWORD -M keepXFF
 
-w2 add /usr/local/lib/node_modules/whistle/.whistle.js
+w2 add --force /usr/local/lib/node_modules/whistle/.whistle.js
 
 tail -f /.dockerenv
