42d0f0e33c13e62b5112b7b47e8b84481440cd94
aogg <8998031+aogg@users.noreply.github.com>
2020-12-14 16:08:05 +0800

    处理if格式错误

--- docker/docker-entrypoint.sshd.sh
+++ docker/docker-entrypoint.sshd.sh
@@ -24,13 +24,13 @@ fi
 
 
 if [ -f /usr/local/bin/dockerd-entrypoint.sh ];then
-    if [ -z "$@"];then
+    if [ -z "$@" ];then
         /usr/local/bin/dockerd-entrypoint.sh
     else
         /usr/local/bin/dockerd-entrypoint.sh "$@"
     fi
 else
-    if [ -z "$@"];then
+    if [ -z "$@" ];then
         /usr/local/bin/docker-entrypoint.sh
     else
         /usr/local/bin/docker-entrypoint.sh "$@"
