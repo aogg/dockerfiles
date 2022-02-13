3761ca77f5d7ba69d0f54a1f40ae17dee732743f
aogg <happy2007xinlan@sina.com>
2022-02-13 21:11:47 +0800

    hyperf-sshd

--- docker/docker-entrypoint.sshd.sh
+++ docker/docker-entrypoint.sshd.sh
@@ -1,25 +1,6 @@
 #!/usr/bin/env ash 
 
-# SSHD
-# generate fresh rsa key
-ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
-# generate fresh dsa key
-ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
-#prepare run dir
-mkdir -p /var/run/sshd
-# prepare config file for key based auth
-sed -i "s/UsePrivilegeSeparation.*/UsePrivilegeSeparation no/g" /etc/ssh/sshd_config
-sed -i "s/UsePAM.*/UsePAM no/g" /etc/ssh/sshd_config
-sed -i "s/\(#\s*\)*PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config
-sed -i "s/\(#\s*\)PasswordAuthentication.*/PasswordAuthentication yes/g" /etc/ssh/sshd_config
-sed -i "s/#AuthorizedKeysFile/AuthorizedKeysFile/g" /etc/ssh/sshd_config
-
-if [ -n "$SSHD_PASSWORD" ];then
-    echo '设置密码';
-    echo root:${SSHD_PASSWORD}|chpasswd
-fi
-
-/usr/sbin/sshd -p ${SSHD_PORT}
+/open-sshd-passwd.sh
 
 
 
