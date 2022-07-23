83f3bfdd934039f68b5ac8dbb1be4513e78704a4
aogg <aogg@users.noreply.github.com>
2022-07-23 18:25:54 +0800

    env

--- nginx/php-git-opcache/docker-entrypoint.sh
+++ nginx/php-git-opcache/docker-entrypoint.sh
@@ -5,12 +5,12 @@
 # 替换nginx访问的php
 cp -f /etc/nginx/conf.d/default.conf.bak /etc/nginx/conf.d/default.conf
 sed -e 's#--php-host-port--#'$PHP_HOST_PORT'#' /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf;
-sed -e 's#--git-dir--#'$GIT_DIR'#' /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf;
+sed -e 's#--git-dir--#'$GIT_DIR_ENV'#' /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf;
 nginx &
 
 
 # 创建用户$GIT_DIR_USER
-/create-user.sh $GIT_DIR_USER
+/create-user.sh $GIT_DIR_USER_ENV
 
 # 监听
 mkdir -p /mnt;
--- nginx/php-git-opcache/inotify-php-git-opcache.sh
+++ nginx/php-git-opcache/inotify-php-git-opcache.sh
@@ -8,17 +8,17 @@ do
       tag=$(cat $file);
       
 
-      cd $GIT_DIR;
+      cd $GIT_DIR_ENV;
 # adduser -u 1000 -D www
 
 
 #su  www /bin/bash  -c "git status;git reset --hard;git clean -df;";
-su  $GIT_DIR_USER /bin/sh  -c "git status;git reset --hard;";
+su  $GIT_DIR_USER_ENV /bin/sh  -c "git status;git reset --hard;";
 
 
 
-su $GIT_DIR_USER /bin/sh  -c "ssh -T -o StrictHostKeyChecking=no ssh.gogs.lkweixin.com || echo 'ssh确认'";
-su $GIT_DIR_USER /bin/sh  -c "git fetch;";
+su $GIT_DIR_USER_ENV /bin/sh  -c "ssh -T -o StrictHostKeyChecking=no ssh.gogs.lkweixin.com || echo 'ssh确认'";
+su $GIT_DIR_USER_ENV /bin/sh  -c "git fetch;";
 
     
     if [[ -n $(git tag | grep -w "$tag")  ]]; then
@@ -30,7 +30,7 @@ exit;
 
 if [ -n "$UPDATE_BEFORE_SHELL_STRING"];then
     echo $UPDATE_BEFORE_SHELL_STRING;
-    su  $GIT_DIR_USER /bin/sh  -c $UPDATE_BEFORE_SHELL_STRING;
+    su  $GIT_DIR_USER_ENV /bin/sh  -c $UPDATE_BEFORE_SHELL_STRING;
 fi;
 
 
@@ -41,19 +41,19 @@ echo '当前标签: '$currentTag
 git diff $currentTag $tag --name-only|sed -e 's#\(.*\)#'$(pwd)'/\1#' > /tmp/git_pull_files.log
 chmod 777 /tmp/git_pull_files.log;
 
-su $GIT_DIR_USER /bin/sh  -c "echo '开始更新代码' && git checkout $tag;git status";
+su $GIT_DIR_USER_ENV /bin/sh  -c "echo '开始更新代码' && git checkout $tag;git status";
 echo '更新后日志: '$(git log --oneline --decorate|grep tag|head -1)
 
 
-cp -f /opcacheUpdate.php $GIT_DIR_USER/opcacheUpdate.php;
-chmod 777 $GIT_DIR_USER/opcacheUpdate.php;
+cp -f /opcacheUpdate.php $GIT_DIR_USER_ENV/opcacheUpdate.php;
+chmod 777 $GIT_DIR_USER_ENV/opcacheUpdate.php;
 curl --data-binary "@/tmp/git_pull_files.log" localhost/opcacheUpdate.php;
-rm -f $GIT_DIR_USER/opcacheUpdate.php;
+rm -f $GIT_DIR_USER_ENV/opcacheUpdate.php;
 
 
 if [ -n "$UPDATE_AFTER_SHELL_STRING"];then
     echo $UPDATE_AFTER_SHELL_STRING;
-    su  $GIT_DIR_USER /bin/sh  -c $UPDATE_AFTER_SHELL_STRING;
+    su  $GIT_DIR_USER_ENV /bin/sh  -c $UPDATE_AFTER_SHELL_STRING;
 fi;
 
 
