8f16a052067de321960fcda3d112f7886b46bac2
aogg <aogg@users.noreply.github.com>
2022-07-25 20:39:01 +0800

    优化错误

--- nginx/php-git-opcache/docker-entrypoint.sh
+++ nginx/php-git-opcache/docker-entrypoint.sh
@@ -4,8 +4,8 @@
 
 # 替换nginx访问的php
 cp -f /etc/nginx/conf.d/default.conf.bak /etc/nginx/conf.d/default.conf
-sed -e 's#--php-host-port--#'$PHP_HOST_PORT'#' /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf;
-sed -e 's#--git-dir--#'$GIT_DIR_ENV'#' /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf;
+sed -ie 's#--php-host-port--#'$PHP_HOST_PORT'#' /etc/nginx/conf.d/default.conf;
+sed -ie 's#--git-dir--#'$GIT_DIR_ENV'#' /etc/nginx/conf.d/default.conf;
 nginx &
 
 
@@ -18,14 +18,14 @@ touch /mnt/git-inotify.txt;
 
 if [ -n "$GIT_AUTO_PULL_FALSE" ];then
 
-/inotify-php-opcache.sh /mnt/git_pull_files.log &
-# 立即更新
-echo '' >> /mnt/git_pull_files.log;
+    /inotify-php-opcache.sh /mnt/git_pull_files.log &
+    # 立即更新
+    echo '' >> /mnt/git_pull_files.log;
 else
 
-/inotify-php-git-opcache.sh /mnt/git-inotify.txt &
-# 立即更新
-echo '' >> /mnt/git-inotify.txt;
+    /inotify-php-git-opcache.sh /mnt/git-inotify.txt &
+    # 立即更新
+    echo '' >> /mnt/git-inotify.txt;
 fi;
 
 
--- nginx/php-git-opcache/inotify-php-git-opcache.sh
+++ nginx/php-git-opcache/inotify-php-git-opcache.sh
@@ -1,32 +1,8 @@
 #!/bin/sh
 
 
-# src=/data/www/
-inotifywait -mrq --timefmt '%d/%m/%y %H:%M' --format '%T %w%f%e' -e modify,delete,create,attrib $1 |  while read file
-do
-      echo "  ${file} inotifywait"
-      tag=$(cat $file);
-      
-
-      cd $GIT_DIR_ENV;
-# adduser -u 1000 -D www
-
-
-#su  www /bin/bash  -c "git status;git reset --hard;git clean -df;";
-su  $GIT_DIR_USER_ENV /bin/sh  -c "git status;git reset --hard;";
-
-
-
-su $GIT_DIR_USER_ENV /bin/sh  -c "ssh -T -o StrictHostKeyChecking=no ssh.gogs.lkweixin.com || echo 'ssh确认'";
-su $GIT_DIR_USER_ENV /bin/sh  -c "git fetch;";
-
-    
-    if [[ -n $(git tag | grep -w "$tag")  ]]; then
-       echo '开始更新';
-    else
-      echo "该标签不存在"
-exit;
-    fi
+function startCode()
+{
 
 if [ -n "$UPDATE_BEFORE_SHELL_STRING"];then
     echo $UPDATE_BEFORE_SHELL_STRING;
@@ -41,20 +17,53 @@ echo '当前标签: '$currentTag
 git diff $currentTag $tag --name-only|sed -e 's#\(.*\)#'$(pwd)'/\1#' > /tmp/git_pull_files.log
 chmod 777 /tmp/git_pull_files.log;
 
-su $GIT_DIR_USER_ENV /bin/sh  -c "echo '开始更新代码' && git checkout $tag;git status";
+su $GIT_DIR_USER_ENV /bin/sh  -c "echo '开始更新代码' && git checkout $1;git status";
 echo '更新后日志: '$(git log --oneline --decorate|grep tag|head -1)
 
 
-cp -f /opcacheUpdate.php $GIT_DIR_USER_ENV/opcacheUpdate.php;
-chmod 777 $GIT_DIR_USER_ENV/opcacheUpdate.php;
-curl --data-binary "@/tmp/git_pull_files.log" localhost/opcacheUpdate.php;
-rm -f $GIT_DIR_USER_ENV/opcacheUpdate.php;
+cp -f /opcacheUpdate.php $GIT_DIR_ENV/opcacheUpdate.php;
+chmod 777 $GIT_DIR_ENV/opcacheUpdate.php;
+curl --data-binary "@/tmp/git_pull_files.log" http://localhost/opcacheUpdate.php;
+rm -f $GIT_DIR_ENV/opcacheUpdate.php;
 
 
 if [ -n "$UPDATE_AFTER_SHELL_STRING"];then
     echo $UPDATE_AFTER_SHELL_STRING;
     su  $GIT_DIR_USER_ENV /bin/sh  -c $UPDATE_AFTER_SHELL_STRING;
 fi;
+}
+
+
+
+inotifywaitFile=$1;
+# src=/data/www/
+inotifywait -mrq --timefmt '%d/%m/%y %H:%M' --format '%T %w%f%e' -e modify,delete,create,attrib $inotifywaitFile |  while read file
+do
+      echo "  ${file} inotifywait"
+      tag=$(cat $inotifywaitFile);
+      
+
+      cd $GIT_DIR_ENV;
+# adduser -u 1000 -D www
+
+
+#su  www /bin/bash  -c "git status;git reset --hard;git clean -df;";
+su  $GIT_DIR_USER_ENV /bin/sh  -c "git status;git reset --hard;";
+
+
+
+# su $GIT_DIR_USER_ENV /bin/sh  -c "ssh -T -o StrictHostKeyChecking=no ssh.gogs.lkweixin.com || echo 'ssh确认'";
+su $GIT_DIR_USER_ENV /bin/sh  -c "git fetch;";
+
+    
+    if [ -n $(git tag | grep -w "$tag")  ]; then
+        echo '开始更新，更新后，标签为--'$tag;
+        startCode $tag;
+    else
+        echo "该标签不存在";
+
+    fi
+
 
 
 done
