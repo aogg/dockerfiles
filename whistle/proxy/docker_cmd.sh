361b1f03a7c7d3bf0f21d094d736458bae8cd9c9
aogg <aogg@users.noreply.github.com>
2022-10-20 16:32:11 +0800

    WHISTLE_MORE_ARGS

--- whistle/proxy/docker_cmd.sh
+++ whistle/proxy/docker_cmd.sh
@@ -11,7 +11,7 @@ sed -i -e "s%#localRules#%`echo $rulesString`%g" /usr/local/lib/node_modules/whi
 
 w2 start $MORE_OPTIONS -p $PROXY_PORT -P $UI_PORT \
     -n $ADMIN_USERNAME -w $ADMIN_PASSWORD \
-    -N $GUEST_USERNAME -W $GUEST_PASSWORD -M keepXFF
+    -N $GUEST_USERNAME -W $GUEST_PASSWORD -M keepXFF $WHISTLE_MORE_ARGS
 
 w2 add --force /usr/local/lib/node_modules/whistle/.whistle.js
 
