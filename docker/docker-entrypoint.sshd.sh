d947a0fe13cbd4d209d52a199451e25b14e6cc7e
aogg <aogg@users.noreply.github.com>
2023-01-12 16:23:41 +0800

    sshd jsh

--- docker/docker-entrypoint.sshd.sh
+++ docker/docker-entrypoint.sshd.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env ash 
 
-/open-sshd-passwd.sh
-
 
 if [ -f /open-sshd-jsh.sh ];then
     /open-sshd-jsh.sh;
     rm /open-sshd-jsh.sh;
 fi
+
+/open-sshd-passwd.sh
+
 
 if [ -f /usr/local/bin/dockerd-entrypoint.sh ];then
     if [ -z "$@" ];then
