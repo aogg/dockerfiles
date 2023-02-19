ec6454ad0107928dbd3cfc7bf4cb20ddf576aa74
aogg <happy2007xinlan@sina.com>
2023-02-19 21:34:43 +0800

    whistle

--- whistle/docker-entrypoint.sh
+++ whistle/docker-entrypoint.sh
@@ -4,9 +4,10 @@ set -e
 echo '数据目录在: /root/.WhistleAppData/.whistle/';
 
 if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
-  w2 start "$@"
+  # w2 start "$@"
 
-  tail -f /.dockerenv
+  # tail -f /.dockerenv
+  exec w2 run "$@"
 fi
 
 exec "$@"
