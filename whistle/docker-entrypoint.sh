f692f8de4295166f36ab254d5fcd87c4e81b6e20
ao-lol <355014+ao-lol@user.noreply.gitee.com>
2023-04-04 11:40:45 +0800

    whistle supervisor

--- whistle/docker-entrypoint.sh
+++ whistle/docker-entrypoint.sh
@@ -3,11 +3,15 @@
 set -e
 echo '数据目录在: /root/.WhistleAppData/.whistle/';
 
+
+
 if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
   # w2 start "$@"
 
   # tail -f /.dockerenv
-  exec w2 run "$@"
+
+  supervisord --user root --logfile /dev/null --pidfile /dev/null -c /etc/supervisord.conf -n
+  # exec w2 run "$@"
 fi
 
 exec "$@"
