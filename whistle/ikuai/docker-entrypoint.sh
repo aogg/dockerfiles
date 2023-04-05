b30f67f9283b7273c28e7580dda091609c6e8b53
ao-lol <355014+ao-lol@user.noreply.gitee.com>
2023-04-05 16:09:01 +0800

    whistle ikuai

---
+++ whistle/ikuai/docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env ash
+
+# set -e
+echo "数据目录在: /root/.WhistleAppData/.whistle/";
+
+
+
+if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
+  # w2 start "$@"
+
+  tail -f /.dockerenv
+
+  # supervisord --user root --logfile /dev/null --pidfile /dev/null -c /etc/supervisord.conf -n
+  # exec w2 run "$@"
+fi
+
+exec "$@"
+
+
+# 必须start
