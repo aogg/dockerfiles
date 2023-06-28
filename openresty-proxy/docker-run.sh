cacd4f4f1c12c1fb9cd32b7a1a043b82ebfe1d04
aogg <happy2007xinlan@sina.com>
2023-06-28 12:00:07 +0800

    exec /usr/local/openresty/bin/openresty -g "daemon off;"

--- openresty-proxy/docker-run.sh
+++ openresty-proxy/docker-run.sh
@@ -3,4 +3,4 @@
 sed -i -e "s%--client_max_body_size--%`echo $CLIENT_MAX_BODY_SIZE`%g" /etc/nginx/conf.d/lua.host.dir.conf
 
 
-exec /usr/local/openresty/bin/openresty -g "daemon off"
+exec /usr/local/openresty/bin/openresty -g "daemon off;"
