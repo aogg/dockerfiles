a05eef801513551434568ed0d2bd830821ee6057
aogg <happy2007xinlan@sina.com>
2023-07-05 19:39:34 +0800

    http_shell     var stderr bytes.Buffer

--- golang/http_shell/http_shell.go
+++ golang/http_shell/http_shell.go
@@ -12,6 +12,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
     // 获取请求参数中的cmd参数值
     // curl "172.17.0.2:8080" -d "cmd-base64=docker ps"
     cmd := r.FormValue("cmd")
+    // 有+号需要请求端urlencode
     cmdBase64 := r.FormValue("cmd-base64")
 
     // curl "172.17.0.2:8080" -d "cmd-base64=$(echo docker ps|base64)"
@@ -35,18 +36,24 @@ func handler(w http.ResponseWriter, r *http.Request) {
     log.Printf("执行的命令: %s", cmd)
 
     // 执行Shell命令
-    out, err := exec.Command("sh", "-c", cmd).Output()
-    if err != nil {
-        out, err = exec.Command(cmd).Output()
-        if err != nil {
-            log.Printf("执行命令报错: %v", err)
-            http.Error(w, fmt.Sprintf("执行命令报错 '%s': %v", cmd, err), http.StatusInternalServerError)
-            return
-        }
+    cmdExec := exec.Command(cmd)
+    var out bytes.Buffer
+    var stderr bytes.Buffer
+    cmdExec.Stdout = &out
+    cmdExec.Stderr = &stderr
 
-        // log.Printf("执行命令报错: %v", err)
-        // http.Error(w, fmt.Sprintf("执行命令报错 '%s': %v", cmd, err), http.StatusInternalServerError)
-        // return
+    err := cmdExec.Run()
+
+    if err != nil {
+        errMsg := stderr.String()
+        outMsg := out.String()
+        log.Printf("执行命令报错: 正常输出: %s 错误：%s", outMsg, errMsg)
+        http.Error(
+            w, 
+            fmt.Sprintf("执行命令报错: 正常输出: %s 错误：%s", outMsg, errMsg), 
+            http.StatusInternalServerError
+        )
+        return
     }
 
     // 将命令输出返回给客户端
