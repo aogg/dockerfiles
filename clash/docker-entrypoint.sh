4d0ee98e8fc488f78a130eb4f3b8f915ab4dc4ce
aogg <8998031+aogg@users.noreply.github.com>
2023-08-06 14:39:30 +0800

    clash 内容为空

--- clash/docker-entrypoint.sh
+++ clash/docker-entrypoint.sh
@@ -13,6 +13,18 @@ EOF
 
 fi
 
+# 内容为空
+if [ ! "$(cat '/root/.config/clash/config.yaml')" ];then 
+    # echo 'mixed-port: 7890' > /root/.config/clash/config.yaml
+      cat > /root/.config/clash/config.yaml <<EOF
+mixed-port: ${PORT:-8090}
+
+# 设置为 true 以允许来自其他 LAN IP 地址的连接
+# allow-lan: true
+EOF
+
+fi
+
 
 exec /clash
 
