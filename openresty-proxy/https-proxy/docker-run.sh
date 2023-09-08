35725a4ffd2b491202f1645792094b1a6d8e8e4e
aogg <8998031+aogg@users.noreply.github.com>
2023-09-08 17:13:09 +0800

    openresty-proxy/https-proxy/docker-run.sh

--- openresty-proxy/https-proxy/docker-run.sh
+++ openresty-proxy/https-proxy/docker-run.sh
@@ -1,7 +1,6 @@
 #!/bin/ash
 
-sed -i -e "s%--client_max_body_size--%`echo $CLIENT_MAX_BODY_SIZE`%g" /etc/nginx/conf.d/default.conf
-sed -i -e "s%--default_proxy_pass--%`echo $DEFAULT_PROXY_PASS`%g" /etc/nginx/conf.d/default.conf
+sed -i -e "s%--client_max_body_size--%`echo $CLIENT_MAX_BODY_SIZE`%g" /etc/nginx/conf.d/lua.https.proxy.conf
 
 
 
