59eb138468cb37c78b77784de481b09ace394e6e
aogg <8998031+aogg@users.noreply.github.com>
2023-09-21 14:40:48 +0800

    DOCKOVPN_SERVER

--- openvpn/DockOvpn/client/docker-entrypoint.sh
+++ openvpn/DockOvpn/client/docker-entrypoint.sh
@@ -4,12 +4,12 @@
 firstBool=""
 if [ ! -f "/vpn/vpn.conf" ];then
   firstBool="1"
-  wget -O /vpn/vpn.conf $DOCKOVPN_SERVER:8080
+  wget -O /vpn/vpn.conf $DOCKOVPN_SERVER
 fi
 
 # 需要tcp
 # http-proxy 192.168.4.1 1080
-if [ -n "firstBool" ];then
+if [ -n "$firstBool" ];then
   if [ -n "$OPENVPN_HTTP_PROXY" ];then
     echo "http-proxy $OPENVPN_HTTP_PROXY" >> /vpn/vpn.conf
   fi
