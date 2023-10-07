d53a293d9af1b8c44a6e0e44ff7301b15ef016d8
aogg <8998031+aogg@users.noreply.github.com>
2023-10-07 09:25:39 +0800

    adbSh=${ADB_PATH:-adb}

--- airtest/wireless_connect.sh
+++ airtest/wireless_connect.sh
@@ -2,6 +2,8 @@
 
 # @see https://github.com/appium/appium-docker-android/blob/master/Appium/wireless_connect.sh
 
+adbSh=${ADB_PATH:-adb}
+
 if [ ! -z "${ANDROID_DEVICES}" ]; then
 	IFS=',' read -r -a array <<<"${ANDROID_DEVICES}"
 	for i in "${!array[@]}"; do
@@ -9,10 +11,10 @@ if [ ! -z "${ANDROID_DEVICES}" ]; then
 		#string contains check
 		if [[ ${connected_devices} != *${array_device}* ]]; then
 			echo "Connecting to: ${array_device}"
-			adb connect ${array_device} >/dev/null 2>/dev/null
+			$adbSh connect ${array_device} >/dev/null 2>/dev/null
 			#Give time to finish connection
 			sleep 2
-			adb devices
+			$adbSh devices
 			echo "Success!"
 		fi
 	done
