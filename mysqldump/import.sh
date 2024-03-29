20d9963c1fc3bea6c6c97d1815410baf50ae0729
aogg <8998031+aogg@users.noreply.github.com>
2024-03-29 14:24:47 +0800

    mysqldump 优化输出

--- mysqldump/import.sh
+++ mysqldump/import.sh
@@ -63,10 +63,10 @@ else
 
         # 循环检测 mysqldump 进程是否存在
         while check_mysqldump_process; do
-                echo "Waiting for mysqldump process to complete..."
+                echo $(date "+%Y-%m-%d %H:%M:%S")"  Waiting for mysqldump process to complete..."${DB_HOST}
                 sleep 1  # 等待 1 秒后重新检测
         done
 
-        echo "mysqldump process has completed."
+        echo $(date "+%Y-%m-%d %H:%M:%S")"  mysqldump process has completed.  "${DB_HOST}
 
 fi
