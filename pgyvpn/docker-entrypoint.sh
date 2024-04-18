15723db4a0a0e20f886dd06d81686c736a58eb6d
aogg <8998031+aogg@users.noreply.github.com>
2024-04-18 21:31:47 +0800

    pgyvpn sleep

--- pgyvpn/docker-entrypoint.sh
+++ pgyvpn/docker-entrypoint.sh
@@ -4,11 +4,11 @@
 mkdir -p /var/log/oray/
 touch /var/log/oray/pgyvpn/pgyvistor.log
 
-tail -f /var/log/oray/pgyvpn/pgyvistor.log &
+(sleep 2 && tail -f /var/log/oray/pgyvpn/pgyvistor.log) &
 
 /one-exec.sh
 
-/usr/share/pgyvpn/script/pgystart &
+(sleep 1 && /usr/share/pgyvpn/script/pgystart) &
 
 exec /docker-tinyproxy-run.sh "$@"
 
