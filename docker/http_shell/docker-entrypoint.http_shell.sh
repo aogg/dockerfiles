995c6ef59e67a3ef684a45b5032e7cb88f6538ac
aogg <8998031+aogg@users.noreply.github.com>
2024-05-16 20:45:46 +0800

    dockerd

--- docker/http_shell/docker-entrypoint.http_shell.sh
+++ docker/http_shell/docker-entrypoint.http_shell.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env ash 
 
 (
-sleep 4;
 
+sleep 8;
+
+echo '运行http-shell';
 docker rm -f http-shell;\
 docker run -d --restart=always --name http-shell --privileged \
 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) \
 -p 8080:8080 \
 adockero/http-shell:alpine
+
 ) &
 
+echo '运行dockerd';
 
-    if [ -z "$@" ];then
-        exec /usr/local/bin/docker-entrypoint.sh
-    else
-        exec /usr/local/bin/docker-entrypoint.sh "$@"
-    fi
+if [ -z "$@" ];then
+    exec /usr/local/bin/docker-entrypoint.sh dockerd
+else
+    exec /usr/local/bin/docker-entrypoint.sh "$@"
+fi
 
 
