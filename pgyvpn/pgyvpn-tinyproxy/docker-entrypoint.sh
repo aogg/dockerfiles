cfada563c5f436e2c06e10cbeabe530f955d74c8
aogg <8998031+aogg@users.noreply.github.com>
2024-05-24 19:24:15 +0800

    pgyvpn-tinyproxy换地方

--- pgyvpn/pgyvpn-tinyproxy/docker-entrypoint.sh
+++ pgyvpn/pgyvpn-tinyproxy/docker-entrypoint.sh
@@ -8,7 +8,8 @@ touch /var/log/oray/pgyvpn/pgyvistor.log
 
 /one-exec.sh
 
-(sleep 1 && /usr/share/pgyvpn/script/pgystart) &
+supervisord -c /etc/supervisord.conf
+# (sleep 1 && /usr/share/pgyvpn/script/pgystart) &
 
 exec /docker-tinyproxy-run.sh "$@"
 
