ee1b32c9015dbd984da2e91bc5a690ef63fbcd6e
aogg <8998031+aogg@users.noreply.github.com>
2024-07-04 17:32:28 +0800

    selenium while

--- selenium/getCookie/docker-cmd.sh
+++ selenium/getCookie/docker-cmd.sh
@@ -6,12 +6,24 @@ wget -t 1 -O /getCookie/stealth.min.js https://raw.githubusercontent.com/require
 
 (/dockerstartup/kasm_default_profile.sh /dockerstartup/vnc_startup.sh /dockerstartup/kasm_startup.sh --wait) &
 
-sleep 7
+# sleep 7
 
-sudo ps -ef > /tmp/ps
-cat /tmp/ps
+while true; do
+  if ps -ef | grep -v grep | grep -q xiccd; then
+    echo "xiccd 进程已经在运行"
+    
+    sudo ps -ef > /tmp/ps
+    cat /tmp/ps
 
-echo "开始执行------getCookie.py"
-exec python3 /getCookie/getCookie.py 2>&1
+    echo "开始执行------getCookie.py"
+    exec python3 /getCookie/getCookie.py 2>&1
+
+    break  # 执行成功后跳出循环
+  else
+    echo "xiccd 进程未找到"
+    sleep 1  # 等待一秒后继续循环
+  fi
+done
+
 
 
