7cb3a6a5c65be4c19a5c7a42a0bdd40d7eac5696
aogg <8998031+aogg@users.noreply.github.com>
2024-07-22 14:06:31 +0800

    pv

---
+++ mysqldump/tables-ssh/dump-import.md5.pv.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+
+DB_USER=${DB_USER:-${MYSQL_ENV_DB_USER}}
+DB_PASS=${DB_PASS:-${MYSQL_ENV_DB_PASS}}
+DB_NAME=${DB_NAME:-${MYSQL_ENV_DB_NAME}}
+DB_HOST=${DB_HOST:-${MYSQL_ENV_DB_HOST}}
+IGNORE_DATABASE=${IGNORE_DATABASE}
+ASYNC_WAIT=${ASYNC_WAIT}
+ASYNC_WAIT_MAX=${ASYNC_WAIT_MAX:-100}
+DUMP_PV=${DUMP_PV:-6m}
+DUMP_WAIT_SECONDS=${DUMP_WAIT_SECONDS:-0.6}
+
+# DUMP_ARGS=
+#  docker build -f ./Dockerfile -t adockero/mysqldump:tables-ssh-pv --build-arg APK_ARG=pv --build-arg FILE_ARG=dump-import.md5.pv.sh  .
+
+
+if [[ ${DB_USER} == "" ]]; then
+        echo "Missing DB_USER env variable"
+        exit 1
+fi
+if [[ ${DB_PASS} == "" ]]; then
+        echo "Missing DB_PASS env variable"
+        exit 1
+fi
+if [[ ${DB_HOST} == "" ]]; then
+        echo "Missing DB_HOST env variable"
+        exit 1
+fi
+
+
+# mysqldump 进程的关键字
+# KEYWORD="mysqldump"
+# KEYWORD="ssh -o"
+# IMPORT_KEYWORD="mysql"
+IFS=',' read -ra IGNORE_PAIRS <<< "$IGNORE_DATABASE_TABLES"
+
+
+STRICT_HOST_KEY_CHECKING=${STRICT_HOST_KEY_CHECKING:no}
+
+# 密码加引用就需要eval
+sshRun=$(echo sshpass -p \'"$SSH_PASSWORD"\' ssh -o "StrictHostKeyChecking=$STRICT_HOST_KEY_CHECKING" $SSH_ARGS $SSH_USER@$SSH_IP)
+# sshRun=$(echo $sshRun)
+echo $sshRun
+
+eval "$sshRun pwd"
+
+if [ $? -eq 0 ]; then
+        echo '开始运行';
+else
+    echo "ssh异常，直接结束."
+    exit 1
+fi
+
+a=$(eval "$sshRun 'if [ -f "/tmp/dump-import-ssh" ];then echo 1;else echo 2;fi'")
+
+firstBool=0
+if [[ "$a" != 1 ]];then
+        #  首次
+        echo '首次';
+        firstBool=1
+fi
+
+# 初始化
+eval "$sshRun bash -c \"pwd && mkdir -p /tmp/dump-import-ssh-diff && rm -Rf /tmp/dump-import-ssh-temp && mkdir -p /tmp/dump-import-ssh-temp/mysql_error_log_dir\""
+
+
+echo '开始循环数据库--下面是执行的命令';
+echo eval "$sshRun 'mysql --user=\"${DB_USER}\" --password=\"${DB_PASS}\" --host=\"${DB_HOST}\" -e \"SHOW DATABASES;\"'"
+
+databases=$(echo "DB_PASS=\"${DB_PASS}\";mysql --user=\"${DB_USER}\" --password=\"\${DB_PASS}\" --host=\"${DB_HOST}\" -e \"SHOW DATABASES;\"" | eval "$sshRun 'bash -s'" | tr -d "| " | grep -v Database)
+echo '开始循环数据库---'$databases;
+
+for db in $databases; do
+    if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] && [[ "$db" != "$IGNORE_DATABASE" ]]; then
+# break;
+
+
+        {
+                echo "Dumping database: $db"
+                # 空文件的md5=d41d8cd98f00b204e9800998ecf8427e
+                command=$(cat << BASH
+DB_PASS="${DB_PASS}";
+mkdir -p /tmp/dump-import-ssh-temp/$db/;
+lastTable='';
+mysqldump --log-error=/tmp/dump-import-ssh-temp/mysql_error_log_dir/$db.log --no-create-info --skip-comments --user="${DB_USER}" --password="\${DB_PASS}" --host="${DB_HOST}" $DUMP_ARGS $db | pv -L $DUMP_PV |grep -e '^INSERT INTO'  | while read -r line; do 
+        table=\$(echo "\$line" | awk -F '\`' '{print \$2}');
+        echo -n "\$line" | md5sum | awk -v table="\$table" '{print table, \$1}' >> /tmp/dump-import-ssh-temp/$db/\$table.md5;
+        if [[ -z "\$lastTable" ]];then
+                lastTable=\$table;
+                echo \$lastTable > /tmp/dump-import-ssh-temp/$db.lastTable.log;
+        fi;
+        if [[ "\$lastTable" != "\$table" ]];then
+                if [[ "\$(md5sum /tmp/dump-import-ssh-temp/$db/\$lastTable.md5)" != "\$(md5sum /tmp/dump-import-ssh-diff/$db/\$lastTable.md5)" ]];then
+                        echo '有差异表名 '\$lastTable;
+                fi
+                lastTable=\$table;
+                echo \$lastTable > /tmp/dump-import-ssh-temp/$db.lastTable.log;
+        fi;
+done;
+
+lastTable=\$(cat /tmp/dump-import-ssh-temp/$db.lastTable.log);
+if [[ -n "\$lastTable" ]];then
+        if [[ -f "/tmp/dump-import-ssh-diff/$db/\$lastTable.md5" ]]
+                if [[ "\$(md5sum /tmp/dump-import-ssh-temp/$db/\$lastTable.md5)" != "\$(md5sum /tmp/dump-import-ssh-diff/$db/\$lastTable.md5)" ]];then
+                        echo '有差异表名 '\$lastTable;
+                fi;
+        else
+                echo '有差异表名 '\$lastTable;
+        fi
+fi
+BASH
+                )
+
+                # printf "%s\n" "$command"
+
+# todo 还要管道导出表
+                time (
+                        printf "%s\n" "$command" | eval "$sshRun 'exec -a \"导出数据库\" bash -s'" | while read -r line; do 
+                                table=$(echo $line | awk '{print $2}')
+                                echo $line;
+                                current_jobs=$(pgrep -f "mysqldump" | wc -l)
+                                current_jobs=$(( $current_jobs + 1 ))
+
+                                if [[ "$current_jobs" -lt "$ASYNC_WAIT_MAX" ]]; then
+                                        {
+                                                echo '进程数小于最大等待数，异步导入';
+                                                time (mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" $DUMP_ARGS $db "$table"  | pv -L $DUMP_PV | mysql --user="${IMPORT_DB_USER}" --password="${IMPORT_DB_PASS}" --host="${IMPORT_DB_HOST}" $IMPORT_ARGS "$db") 
+                                                echo "导入结束  $db.$table";
+                                        } &
+                                        sleep $DUMP_WAIT_SECONDS;
+                                else
+                                        echo '进程数大于最大等待数，同步等待导入';
+                                        time (mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" $DUMP_ARGS $db "$table"  | pv -L $DUMP_PV | mysql --user="${IMPORT_DB_USER}" --password="${IMPORT_DB_PASS}" --host="${IMPORT_DB_HOST}" $IMPORT_ARGS "$db") 
+                                        echo "导入结束  $db.$table";
+                                fi
+                                
+                        done;
+                 )
+                # time (eval "$sshRun 'bash -s'" < $command)
+
+
+
+                
+                echo "异步导出库--结束--$db";
+
+
+                # 
+        }
+        # } &
+exit;
+    fi
+done
+
+
+# 检测 mysqldump 进程是否存在的函数
+check_mysqldump_process() {
+        # 使用 pgrep 命令查找与关键字匹配的进程 ID
+        pgrep -f "(ssh|mysql)" >/dev/null 2>&1
+}
+
+# 循环检测 mysqldump 进程是否存在
+while check_mysqldump_process; do
+        echo $(date "+%Y-%m-%d %H:%M:%S")" 最后导入 last  Waiting for mysqldump process to complete..."${DB_HOST}
+        sleep 1  # 等待 1 秒后重新检测
+done
+
+echo $(date "+%Y-%m-%d %H:%M:%S")" 最后导入 last  mysqldump process has completed.  "${DB_HOST}
+
+# 运行成功就存储到diff
+eval "$sshRun bash -c \"pwd && rm -Rf /tmp/dump-import-ssh-diff && mv /tmp/dump-import-ssh-temp /tmp/dump-import-ssh-diff\""
+
+
+echo $(date "+%Y-%m-%d %H:%M:%S")'-------全部结束--------'
