cb24ebc9abe62132a931648ae9797b854b4627e6
aogg <8998031+aogg@users.noreply.github.com>
2024-08-22 21:05:35 +0800

    pgyvpn  bug

--- pgyvpn/ubuntu/docker-entrypoint.sh
+++ pgyvpn/ubuntu/docker-entrypoint.sh
@@ -4,7 +4,9 @@
 mkdir -p /var/log/oray/
 touch /var/log/oray/pgyvpn/pgyvistor.log
 
-(sleep 2 && tail -f /var/log/oray/pgyvpn/pgyvistor.log) &
+# 只有pgyvpn
+(sleep 2 && tail -f /var/log/oray/pgyvpn/pgyvistor.log | awk '{print "/var/log/oray/pgyvpn/pgyvistor.log--文件输出: " $0}') &
+(sleep 2 && tail -f /var/log/oray/pgyvpn_svr/pgyvpnsvr.log | awk '{print "/var/log/oray/pgyvpn_svr/pgyvpnsvr.log--文件输出: " $0}') &
 
 service pgyvpn start
 sleep 1
