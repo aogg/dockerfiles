fd9f25dcc0b36451819603e8fc217bf0d7fcdb61
aogg <8998031+aogg@users.noreply.github.com>
2024-08-22 20:48:23 +0800

    pgyvpn日志优化

--- pgyvpn/pgyvpn-tinyproxy/docker-entrypoint.sh
+++ pgyvpn/pgyvpn-tinyproxy/docker-entrypoint.sh
@@ -4,7 +4,8 @@
 mkdir -p /var/log/oray/
 touch /var/log/oray/pgyvpn/pgyvistor.log
 
-(sleep 2 && tail -f /var/log/oray/pgyvpn/pgyvistor.log) &
+(sleep 2 && tail -f /var/log/oray/pgyvpn/pgyvistor.log | awk '{print "/var/log/oray/pgyvpn/pgyvistor.log--文件输出: " $0}') &
+(sleep 2 && tail -f /var/log/oray/pgyvpn_svr/pgyvpnsvr.log | awk '{print "/var/log/oray/pgyvpn_svr/pgyvpnsvr.log--文件输出: " $0}') &
 
 /one-exec.sh
 
