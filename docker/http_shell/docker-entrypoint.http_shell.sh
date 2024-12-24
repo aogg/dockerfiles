ebfea09df765fb1ba709cd7a77529d22a2e08c72
aogg <aogg@users.noreply.github.com>
2024-12-24 18:38:28 +0800

    Update docker-entrypoint.http_shell.sh

--- docker/http_shell/docker-entrypoint.http_shell.sh
+++ docker/http_shell/docker-entrypoint.http_shell.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env ash 
 
+HTTP_SHELL_PORT=${HTTP_SHELL_PORT:-8080}
+echo "HTTP_SHELL_PORT=$HTTP_SHELL_PORT"
+
 (
 
 sleep 8;
+docker network create common-all
 
 echo '运行http-shell';
 docker rm -f http-shell;\
-docker run -d --restart=always --name http-shell --privileged \
+docker run -d --restart=always --network common-all --name http-shell --privileged \
 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) \
--p 8080:8080 \
+-p $HTTP_SHELL_PORT:8080 \
 adockero/http-shell:alpine
 
 ) &
