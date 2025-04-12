551ca776d1bee2385683e7d7435b2dd9ff3b586e
aogg <aogg@users.noreply.github.com>
2025-04-12 14:14:26 +0800

    pgyvpn

--- pgyvpn/pgyvpn-2.2.2/docker-entrypoint.sh
+++ pgyvpn/pgyvpn-2.2.2/docker-entrypoint.sh
@@ -12,6 +12,14 @@ tail -f /var/log/oray/pgyvpn/pgyvpn.log &
 # /usr/share/pgyvpn/script/pgystart &
 /usr/share/pgyvpn/script/pgyvpn_monitor &
 
+
+# 替换配置
+if [ -n "$TINYPROXY_PROXY" ];then
+    sed -i -e "s%upstream http.*%upstream http `echo $TINYPROXY_PROXY`%g" /etc/tinyproxy/tinyproxy-proxy.conf
+fi
+(/usr/sbin/tinyproxy -c /etc/tinyproxy/tinyproxy-proxy.conf)
+
+
 exec /docker-tinyproxy-run.sh "$@"
 
 
