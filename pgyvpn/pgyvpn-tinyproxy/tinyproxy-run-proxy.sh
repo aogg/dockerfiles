3bb99d830e70493675de2485ce178059f1104dd7
aogg <aogg@users.noreply.github.com>
2025-04-17 14:12:36 +0800

    多个pgyvpn  端口

--- pgyvpn/pgyvpn-tinyproxy/tinyproxy-run-proxy.sh
+++ pgyvpn/pgyvpn-tinyproxy/tinyproxy-run-proxy.sh
@@ -11,6 +11,10 @@ for proxy in "${proxies[@]}"; do
     if [ -n "$proxy" ];then
         cp /etc/tinyproxy/tinyproxy-proxy.conf /etc/tinyproxy/tinyproxy-proxy-${i}.conf
         sed -i -e "s%upstream http.*%upstream http ${proxy}%g" /etc/tinyproxy/tinyproxy-proxy-${i}.conf
+        port=$(expr 8 - $i)
+        echo "端口$port"
+        sed -i -e "s%Port *%Port 888${port}%g" /etc/tinyproxy/tinyproxy-proxy-${i}.conf
+
         
 (/usr/sbin/tinyproxy -c /etc/tinyproxy/tinyproxy-proxy-${i}.conf)
     fi
