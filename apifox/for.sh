6647669c8c8a8c6be741f977f025bb29fcbb3ed0
aogg <aogg@users.noreply.github.com>
2025-05-12 16:50:05 +0800

    apifox_run_i

--- apifox/for.sh
+++ apifox/for.sh
@@ -9,6 +9,7 @@ wait_num=${WAIT:-3}
 current_time=$(date +%s)
 # 计算等待时间后的时间戳
 wait_time=$((current_time + ${wait_num}))
+currentDatetimeDir=/data/apifox/$(date "+%Y-%m-%d--%H-%M-%S")
 
 # 定义并发执行函数
 run_command() {
@@ -31,15 +32,14 @@ run_command() {
 
 
     local num=$2
-    apifox run ${APIFOX_URL} -n $num -r html,cli --verbose --out-dir $1
+    apifox run ${APIFOX_URL} --env-var "apifox_run_i=$1" -n $num -r html,cli --verbose --out-dir $currentDatetimeDir/$1
 }
 
 # 获取环境变量num的值，如果未设置则默认为30
-currentDatetimeDir=/data/apifox/$(date "+%Y-%m-%d--%H-%M-%S")
 # 循环指定次数，将命令放入后台执行
 for ((i = 1; i <= fork_num; i++)); do
     mkdir -p $currentDatetimeDir/$1
-    (run_command $currentDatetimeDir/$1 $for_num) &
+    (run_command $1 $for_num) &
 done
 
 
