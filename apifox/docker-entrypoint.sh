a23797cd41867062e247ebfd010cc216412e4622
aogg <aogg@users.noreply.github.com>
2025-06-26 10:35:08 +0800

    apifox

--- apifox/docker-entrypoint.sh
+++ apifox/docker-entrypoint.sh
@@ -3,4 +3,10 @@
 
 echo ${APIFOX_URL}
 
+echo "apifox版本"
+apifox -v
+
+echo "最终运行命令"
+echo apifox run ${APIFOX_URL} -r html,cli --verbose --out-dir /data/apifox "$@"
+
 exec apifox run ${APIFOX_URL} -r html,cli --verbose --out-dir /data/apifox "$@"
