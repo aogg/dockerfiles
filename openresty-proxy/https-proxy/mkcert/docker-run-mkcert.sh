c8d35bc394720ea731be2b05e13d254f28cacc9a
aogg <2016teemo@gmail.com>
2025-10-10 17:57:17 +0800

    在 mkcert 脚本中添加 chmod 命令，以确保复制到目标目录的证书文件具有适当的权限

--- openresty-proxy/https-proxy/mkcert/docker-run-mkcert.sh
+++ openresty-proxy/https-proxy/mkcert/docker-run-mkcert.sh
@@ -7,5 +7,7 @@ ls -al /root/.local/share/mkcert/;
 # rm -f /etc/nginx/ssl/mkcert/rootCA-key.pem;
 mkdir -p /mkcert-pem/;
 cp -a /root/.local/share/mkcert/* /mkcert-pem/;
+chmod 666 /mkcert-pem/*;
+
 
 exec /docker-run.sh
