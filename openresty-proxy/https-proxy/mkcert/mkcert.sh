70c0c9bbd87a62748ef773425bef871ca30eee06
aogg <2016teemo@gmail.com>
2025-10-10 17:38:47 +0800

    在 mkcert 脚本中添加 CAROOT 环境变量的导出

--- openresty-proxy/https-proxy/mkcert/mkcert.sh
+++ openresty-proxy/https-proxy/mkcert/mkcert.sh
@@ -3,6 +3,7 @@
 # mkcert -install;
 # cd /root/.local/share/mkcert/;
 
+export CAROOT="/etc/nginx/ssl/mkcert/";
 hostName=$1
 /usr/local/bin/mkcert -key-file  "/etc/nginx/ssl/${hostName}.key" -cert-file  "/etc/nginx/ssl/${hostName}.crt" "${hostName}";
 #  cat rootCA.pem  ${hostName}.crt >> certificate.crt
