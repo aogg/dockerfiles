1e7108d9a130a579bd09a774371cf491a5ca0dc7
aogg <2016teemo@gmail.com>
2025-10-10 17:53:15 +0800

    修复 mkcert 脚本中的证书复制逻辑，确保正确复制所有证书文件到目标目录

--- openresty-proxy/https-proxy/mkcert/docker-run-mkcert.sh
+++ openresty-proxy/https-proxy/mkcert/docker-run-mkcert.sh
@@ -6,6 +6,6 @@ ls -al /root/.local/share/mkcert/;
 # rm -f /etc/nginx/ssl/mkcert/rootCA.pem;
 # rm -f /etc/nginx/ssl/mkcert/rootCA-key.pem;
 mkdir -p /mkcert-pem/;
-cp -a /root/.local/share/mkcert/ /mkcert-pem/;
+cp -a /root/.local/share/mkcert/* /mkcert-pem/;
 
 exec /docker-run.sh
