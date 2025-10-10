861ce117327fe761021e32637c3cf41d3acdd44b
aogg <2016teemo@gmail.com>
2025-10-10 17:46:07 +0800

    移除旧的 mkcert 根证书和密钥文件

--- openresty-proxy/https-proxy/mkcert/docker-run-mkcert.sh
+++ openresty-proxy/https-proxy/mkcert/docker-run-mkcert.sh
@@ -3,6 +3,8 @@
 
 mkcert -install;
 ls -al /root/.local/share/mkcert/;
+rm -f /etc/nginx/ssl/mkcert/rootCA.pem;
+rm -f /etc/nginx/ssl/mkcert/rootCA-key.pem;
 cp -a /root/.local/share/mkcert/ /etc/nginx/ssl/;
 
 exec /docker-run.sh
