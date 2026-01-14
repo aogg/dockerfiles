8fd84688975877f35027dd46499ee18c19384f31
aogg <2016teemo@gmail.com>
2026-01-14 17:10:12 +0800

    将日志清理过程放入后台执行

--- tinyproxy/clean/docker-entrypoint.sh
+++ tinyproxy/clean/docker-entrypoint.sh
@@ -3,10 +3,12 @@
 
 
 # 每分钟清理log
-while true; do
-    tail -n 1000 /var/log/tinyproxy/tinyproxy.log > /var/log/tinyproxy/tinyproxy.log.tmp
-    cat /var/log/tinyproxy/tinyproxy.log.tmp > /var/log/tinyproxy/tinyproxy.log
-    sleep 60
-done
+{
+    while true; do
+        tail -n 1000 /var/log/tinyproxy/tinyproxy.log > /var/log/tinyproxy/tinyproxy.log.tmp
+        cat /var/log/tinyproxy/tinyproxy.log.tmp > /var/log/tinyproxy/tinyproxy.log
+        sleep 60
+    done
+} &
 
 exec /opt/docker-tinyproxy/run.sh $@
