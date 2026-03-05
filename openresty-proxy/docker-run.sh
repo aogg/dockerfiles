c584cd47e71d486239ecc59678f646cb784a7d17
aogg <2016teemo@gmail.com>
2026-03-05 08:56:09 +0800

    PROXY_TIMEOUT

--- openresty-proxy/docker-run.sh
+++ openresty-proxy/docker-run.sh
@@ -1,6 +1,7 @@
 #!/bin/ash
 
 sed -i -e "s%--client_max_body_size--%`echo $CLIENT_MAX_BODY_SIZE`%g" /etc/nginx/conf.d/lua.host.dir.conf
+sed -i -e "s%--proxy_timeout--%`echo $PROXY_TIMEOUT`%g" /etc/nginx/conf.d/lua.host.dir.conf
 
 
 exec /usr/local/openresty/bin/openresty -g "daemon off;"
--- openresty-proxy/https-proxy/docker-run.sh
+++ openresty-proxy/https-proxy/docker-run.sh
@@ -1,6 +1,7 @@
 #!/bin/ash
 
 sed -i -e "s%--client_max_body_size--%`echo $CLIENT_MAX_BODY_SIZE`%g" /etc/nginx/conf.d/lua.https.proxy.conf
+sed -i -e "s%--proxy_timeout--%`echo $PROXY_TIMEOUT`%g" /etc/nginx/conf.d/lua.https.proxy.conf
 
 
 
