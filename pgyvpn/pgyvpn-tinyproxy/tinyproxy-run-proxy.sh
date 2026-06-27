b46e663fbe4d824e72c617849d84a084cb85935e
aogg <happy2007xinlan@sina.com>
2026-06-27 19:38:39 +0800

    pgyvpn-tinyproxy 优化说明 8开始

--- pgyvpn/pgyvpn-tinyproxy/tinyproxy-run-proxy.sh
+++ pgyvpn/pgyvpn-tinyproxy/tinyproxy-run-proxy.sh
@@ -6,20 +6,21 @@ touch /var/log/tinyproxy/tinyproxy-proxy.log
 IFS=',' read -ra proxies <<< "$TINYPROXY_PROXY"
 i=0
 for proxy in "${proxies[@]}"; do
-((i++))
 
     if [ -n "$proxy" ];then
         cp /etc/tinyproxy/tinyproxy-proxy.conf /etc/tinyproxy/tinyproxy-proxy-${i}.conf
         sed -i -e "s%upstream http.*%upstream http ${proxy}%g" /etc/tinyproxy/tinyproxy-proxy-${i}.conf
         port=$(expr 8 - $i)
-        echo "端口$port"
+        echo "端口888$port    代理${proxy}"
         sed -i -e "s%Port .*%Port 888${port}%g" /etc/tinyproxy/tinyproxy-proxy-${i}.conf
 
-        
+        echo "启动 /etc/tinyproxy/tinyproxy-proxy-${i}.conf"
 (/usr/sbin/tinyproxy -c /etc/tinyproxy/tinyproxy-proxy-${i}.conf)
     fi
     # 这里可以添加针对每个代理地址的具体操作，比如使用 curl 测试连通性
     # curl --proxy "$proxy" http://example.com
+    
+((i++))
 done
 
 
