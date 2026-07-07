3a3ae580b9edeea1455343aac13ef4fac2b18540
aogg <happy2007xinlan@sina.com>
2026-07-07 20:16:59 +0800

    dind exec

--- docker/docker-entrypoint.sshd.sh
+++ docker/docker-entrypoint.sshd.sh
@@ -11,15 +11,15 @@ fi
 
 if [ -f /usr/local/bin/dockerd-entrypoint.sh ];then
     if [ -z "$@" ];then
-        /usr/local/bin/dockerd-entrypoint.sh
+        exec /usr/local/bin/dockerd-entrypoint.sh
     else
-        /usr/local/bin/dockerd-entrypoint.sh "$@"
+        exec /usr/local/bin/dockerd-entrypoint.sh "$@"
     fi
 else
     if [ -z "$@" ];then
-        /usr/local/bin/docker-entrypoint.sh
+        exec /usr/local/bin/docker-entrypoint.sh
     else
-        /usr/local/bin/docker-entrypoint.sh "$@"
+        exec /usr/local/bin/docker-entrypoint.sh "$@"
     fi
 fi
 
