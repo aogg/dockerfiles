ef3a3c3f89014c5a944e3f886ea4bac9dd4f7bd6
aogg <happy2007xinlan@sina.com>
2026-07-15 17:14:13 +0800

    hermes 分开启动ui

--- hermes/docker-entrypoint.sh
+++ hermes/docker-entrypoint.sh
@@ -5,6 +5,9 @@ if [ ! -d "/opt/data" ]; then
     mv /opt/data.bak /opt/data
 fi
 
-hermes-web-ui start &
+# hermes-web-ui start &
+
+
+(sleep 4 && cd $HERMES_ORIG_CWD && su -m hermes -c 'hermes dashboard --host 0.0.0.0 --port 9119 --no-open --insecure') &
 
 exec /opt/hermes/docker/main-wrapper.sh "$@"
